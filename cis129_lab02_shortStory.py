1a419f9dbfd71228f29f6ca6a94ca851e9b5264c
apahceco <159049172+apahceco@users.noreply.github.com>
2024-02-07 16:11:24 -0700

    Update cis129_lab02_shortStory.py
    The changes were made for Lab02 for Programming Problem Solving 1.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,16 @@
-print('Hello, World!')
+# Author:Anthony Pacheco 
+# My first Python lab in my CIS129 course
+# This program should prints a simple short story about Anthony Pacheco.
+
+
+print('This is my first project in python.')
+
+print('I am really excited to learn more about python.')
+
+print('That way I can one day create my own AI to better security for all applications.') 
+
+print('So the user doesnt have to worry about their data anymore.') 
+
+print('Any suggestion from anyone is welcome')  
+
+print('Thank you\, have a great day!')
